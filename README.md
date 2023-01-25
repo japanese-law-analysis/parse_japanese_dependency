ac93789ed922e57abe147c1f23f2f34845ac2ba1
puripuri2100 <puripuri2100@gmail.com>
2023-01-26 04:17:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# parse_japanese_dependency
+
+[Ginza](https://megagonlabs.github.io/ginza/)を用いて係り受けの解析を行うソフトウェアです。
+
+```
+python parse_japanese_dependency.py --input input.json --output output.json
+```
+
+で使用できます。
+
+`input.json`には文と文のIDを書きます。書き方は`test.json`を見てください。
+
+また、ライブラリとしては解析対象のテキストとモデルを受け取って解析結果の辞書情報を返す`parse_document`関数が使用できます。
+
+
+# インストール
+
+依存ライブラリは
+
+- spacy
+- ginza
+- ja_ginza
+
+です。
+
+詳しくは[Ginza](https://megagonlabs.github.io/ginza/)の「実行環境のセットアップ/2. GiNZA + 従来型モデル」の節などを確認してください。
+
---
+++ parse_japanese_dependency.py
@@ -0,0 +1,59 @@
+import spacy
+import ginza
+import json
+import argparse
+
+
+def parse_document(sentence, nlp):
+    doc = nlp(sentence)
+    token_info = {}
+    len_sum = 0
+    for sent in doc.sents:
+        for token in sent:
+            i = str(token.i)
+            token_len = len(token.text)
+            token_info[i] = { "start":len_sum, "end": len_sum + token_len - 1, "text": token.text }
+            len_sum = len_sum + token_len
+
+    # 係受けの情報を追加する
+    for span in ginza.bunsetu_spans(doc):
+        for token in span.lefts:
+            i = str(token.i)
+            d = token_info[i]
+            d["head_start"] = span.start
+            d["head_end"] = span.end
+            token_info[i] = d
+    return token_info
+
+parser = argparse.ArgumentParser(prog = "parse japanese dependency", description = "係り受け解析")
+parser.add_argument("-i", "--input")
+parser.add_argument("-o", "--output")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    input_file_name = args.input
+    output_file_name = args.output
+    input_f = open(input_file_name, "r", encoding="UTF-8")
+    input_s = input_f.read()
+    input_f.close()
+    input_data = json.loads(input_s)
+    nlp = spacy.load('ja_ginza')
+    output_s = "{"
+    is_head = True
+    for k,v in input_data.items():
+        print(k)
+        d = parse_document(v, nlp)
+        if is_head:
+            output_s = output_s + "\n"
+            is_head = False
+        else:
+            output_s = output_s + ",\n"
+        s = json.dumps(d, ensure_ascii=False)
+        output_s = output_s + f"\"{k}\":{s}"
+    output_s = output_s + "\n}"
+    output_f = open(output_file_name, "w", encoding="UTF-8")
+    output_f.write(output_s)
+    output_f.close()
+
+
